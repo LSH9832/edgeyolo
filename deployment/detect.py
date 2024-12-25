6d004d829f841b803442c63fe4c138459b62de80
SHL <69880398+LSH9832@users.noreply.github.com>
2024-12-25 19:25:49 +0800

    Update detect.py

--- deployment/detect.py
+++ deployment/detect.py
@@ -15,6 +15,12 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", type=str, default="models/agv.yaml")
     parser.add_argument("-s", "--source", type=str, required=True)
+    
+    parser.add_argument("--eval", action="store_true", help="evaluate from source")
+    
+    parser.add_argument("--number", type=int, default=-1, help="just test number of images and then exit, set -1 to disable, default is -1")
+    
+    
     parser.add_argument("-t", "--topic", type=str, default=None)
     parser.add_argument("--no-show", action="store_true")
     parser.add_argument("--dist", type=str, default="output.mp4")
@@ -33,8 +39,19 @@ def main():
 
     cfg = yaml.load(open(args.config), yaml.Loader)
     model_path = osp.join(osp.dirname(args.config), cfg["model_path"])
-    show = not args.no_show
+    show = not args.no_show and not args.eval
     # print(model_path)
+    
+    eval_mode = args.eval
+    max_number = args.number
+    
+    kwargs = cfg.get("kwargs", {})
+    if eval_mode:
+        json_results = {}
+        kwargs["conf_threshold"] = 0.001
+        kwargs["nms_threshold"] = 0.65
+        kwargs["show_info"] = False
+        assert osp.isdir(args.source), "when use eval mode, image source should be a dir with images for some dataset"
     
     model = YOLO(
         model_path,
@@ -46,7 +63,7 @@ def main():
         cfg.get("normalize", False),
         cfg.get("strides", [8, 16, 32]),
         cfg.get("device", 0),
-        **cfg.get("kwargs", {})
+        **kwargs
     )
 
     print(f"\033[32m\033[1m[INFO] acceleration platform: {model.platform()}\033[0m")
@@ -58,7 +75,7 @@ def main():
     if not cap.isOpened():
         print("failed to open capture")
         exit(-1)
-    if not show:
+    if not show and not eval_mode:
         fps = args.fps if args.fps > 0 else cap.get(cv2.CAP_PROP_FPS)
         if fps < 1:
             fps = 30
@@ -91,8 +108,10 @@ def main():
         writer_thread = Thread(target=writeVideo, args=(args.dist, cv2.VideoWriter_fourcc(*'mp4v'), fps, (imgw, imgh), True))
         writer_thread.start()
             
-        
+    elif eval_mode:
+        num_imgs = len(cap.imgs)
     try:
+        count = 0
         while cap.isOpened():
 
             success, frame = cap.read()
@@ -102,11 +121,18 @@ def main():
             if not all(frame.shape):
                 break
             
+            count += 1
+            if count == max_number:
+                break
             t0 = time()
             result = model.infer(frame)
-            print(f"\rinfer delay: {(time()-t0) * 1000:.3f}ms, num result={len(result)} ", end="             ")
+            if eval_mode:
+                print(f"\reval: {count}/{num_imgs}", end="    ")
+            else:
+                print(f"\rinfer delay: {(time()-t0) * 1000:.3f}ms, num result={len(result)} ", end="             ")
 
-            img_show = draw(frame, [result], model.names, 2)
+            if not eval_mode:
+                img_show = draw(frame, [result], model.names, 2)
 
             if show:
                 cv2.imshow("test", img_show)
@@ -115,16 +141,33 @@ def main():
                     delay = 1 - delay
                 elif key == 27:
                     break
-            else:
+            elif not eval_mode:
                 imgs_list.append(img_show)
+            else:
+                if json_results.get(cap.currentFile, None) is None:
+                    json_results[cap.currentFile] = []
+                for x1, y1, x2, y2, cls, conf in result:
+                    json_results[cap.currentFile].append({
+                        "category_id": int(cls),
+                        "bbox": [float(x1), float(y1), float(x2 - x1), float(y2 - y1)],
+                        "score": float(conf),
+                        "segmentation": [],
+                    })
+                
+                
+                
     finally:
         cv2.destroyAllWindows()
         model.release()
         print()
-        if not show:
+        if not show and not eval_mode:
             print("wait", len(imgs_list), "images to write to ", args.dist)
             imgs_list.append(None)
             writer_thread.join()
+        elif eval_mode:
+            import json
+            with open("eval_results.json", "w") as f:
+                json.dump(json_results, f)
         
 
 
